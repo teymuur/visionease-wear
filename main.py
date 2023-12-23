3a3bf2b6e40f167b133d38aff3cf50a02a7cbfb0
teymuur <teymur_babayev08@yahoo.com>
2023-12-23 19:23:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -23,10 +23,16 @@ if not cap.isOpened():
     cap = cv2.VideoCapture(1)
 if not cap.isOpened():
     raise IOError("cannot open")
+def detect_bounding_box(vid):
+    gray_image = cv2.cvtColor(vid, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(vid, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return faces
 
 while True:
 
-    result, video_frame = video_capture.read()  # read frames from the video
+    result, video_frame = cap.read()  # read frames from the video
     if result is False:
         break  # terminate the loop if the frame is not read successfully
 
