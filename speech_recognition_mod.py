b95d4e4521cb4d6ddaa9b899216d88781c689b79
teymuur <teymur_babayev08@yahoo.com>
2023-12-28 01:01:57 +0400

    Added voice assitant functions

--- speech_recognition_mod.py
+++ speech_recognition_mod.py
@@ -1,7 +1,9 @@
 import speech_recognition
 import pyttsx3
+import requests
 # Text to speech
 text_speech = pyttsx3.init()
+text_speech.setProperty("rate",180)
 def speak(answer):
     text_speech.say(answer)
     text_speech.runAndWait()
@@ -24,6 +26,27 @@ def __listen__():
         except Exception :
             recognizer = speech_recognition.Recognizer()
             continue
+def getweather():
+    api_key = open('weather_api_key.txt', 'r').read()
+    while True:
+        
+        location = "Baku"
+
+        result = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={api_key}')
+        if result.json()['cod'] == '404':
+            print("Invalid location!")
+            continue
+        break
+    
+    description = result.json()['weather'][0]['description']
+    temperature = round(result.json()['main']['temp'])
+    feels_like = round(result.json()['main']['feels_like'])
+    high = round(result.json()['main']['temp_max'])
+    low = round(result.json()['main']['temp_min'])
+
+    speak(f"The weather in {location[0].upper()}{location[1:]} is {temperature}° C with {description}.")
+    speak(f"It feels like {feels_like}° C.")
+    speak(f"Today's high is {high}° C and today's low is {low}° C.")
 def listen():
     while True:
         t  = __listen__()
@@ -31,4 +54,10 @@ def listen():
             break
         elif "for all my" in t and "know me" in t:
             speak("I feel like me and Taylor might still have sex")
+            speak("Why .. I made that bitch famous... Goddamn")
+        elif "w men's" in t:
+            speak("Hell yeah brother")
+        elif "weather" in t: 
+            getweather()
+
 print(listen())
