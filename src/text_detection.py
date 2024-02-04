92ec8d5be9e9725dcdda0b74093e8b4dfaa17cd6
teymuur <teymur_babayev08@yahoo.com>
2024-02-04 19:24:15 +0400

    Updated text_detection

--- src/text_detection.py
+++ src/text_detection.py
@@ -1,17 +1,24 @@
 import pytesseract
+import speech_recognition_mod as sr
+import cv2
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-import speech_recognition_mod as sr
+cap = cv2.VideoCapture(1)
 
 def extract_text_from_image(image):
     # Use pytesseract to extract text from the image
     while sr.loop_flag:
         return pytesseract.image_to_string(image, config='--psm 1')
 
-def process_frame_with_ocr():
-    global frame
-    text_from_ocr = extract_text_from_image(frame)
-     # Print or use the OCR result as needed
-    print(f"OCR Result from Entire Image: {text_from_ocr}")
-    sr.speak(f"Reading text: {text_from_ocr}")
+def process_frame_with_ocr(image):
+    while True:
+        image = cap.read()
+        text_from_ocr = extract_text_from_image(image)
+        # Print or use the OCR result as needed
+        print(f"OCR Result from Entire Image: {text_from_ocr}")
+        sr.speak(f"Reading text: {text_from_ocr}")
+
+
+if __name__ == "__main__":
+    process_frame_with_ocr(cap)
