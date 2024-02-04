fcfe52c2bc2db929f56d814b7f5e70cddea86f76
teymuur <teymur_babayev08@yahoo.com>
2024-02-04 19:00:27 +0400

    Updated qr

--- src/qr_detection.py
+++ src/qr_detection.py
@@ -2,23 +2,34 @@ import cv2
 from pyzbar.pyzbar import decode
 import numpy as np
 import speech_recognition_mod as sr
+import time
 
 cap = cv2.VideoCapture(1)
 cap.set(3,640)
 cap.set(4,480)
+
+
+scanned_qr_codes = {}
 
 while True:
 
     success, img = cap.read()
     for barcode in decode(img):
         myData = barcode.data.decode('utf-8')
-        print(myData)
         pts = np.array([barcode.polygon],np.int32)
         pts = pts.reshape((-1,1,2))
         cv2.polylines(img,[pts],True,(255,0,255),5)
         pts2 = barcode.rect
         cv2.putText(img,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX, 0.9,(255,0,255),2)
-        sr.speak()
+        print(f"QR Code Decoded: {myData}")
+
+        if myData not in scanned_qr_codes or (time.time() - scanned_qr_codes[myData]) > 300:
+
+            sr.speak(myData)
+            print(f"QR Code Decoded: {myData}")
+
+            # Update the scanned QR codes list with the current time
+            scanned_qr_codes[myData] = time.time()
 
     cv2.imshow('Result',img)
     cv2.waitKey(1)
