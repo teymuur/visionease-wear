9702934caf1acceb14c15019f911cfe43c2b4568
teymuur <teymur_babayev08@yahoo.com>
2024-02-04 18:43:57 +0400

    Updated qr

--- src/qr_detection.py
+++ src/qr_detection.py
@@ -1,18 +1,20 @@
 import cv2
-import numpy as np
 from pyzbar.pyzbar import decode
-
+import speech_recognition as sr
 
 cap = cv2.VideoCapture(0)
 cap. set (3,640)
 cap.set(4,480)
 
-while True:
-     success, img = cap.read()
-     for barcode in decode(img):
-        print(barcode. data)
-        myData = barcode.data.decode('utf-8')
-        print(myData)
-     cv2. imshow('Result',img)
-     cv2.waitKey (1)
+def qr():
+    while True:
+        success, img = cap.read()
+        for barcode in decode(img):
+            print(barcode. data)
+            myData = barcode.data.decode('utf-8')
+            print(myData)
+        cv2. imshow('Result',img)
+        cv2.waitKey(1)
 
+if __name__ == "__name__":
+    qr()
