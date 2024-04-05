c6861475ad5e187b1cf2c165f2a8509de07408ca
teymuur <teymur_babayev08@yahoo.com>
2024-04-05 23:36:46 +0400

    Translated

--- src/translate.py
+++ src/translate.py
@@ -4,19 +4,19 @@ import os
 from playsound import playsound
 from googletrans import Translator
 
-def translate(text, target_language='tr'):
+def translate(text, target_language='ar'):
     translator = Translator()
     translation = translator.translate(text, dest=target_language)
     return translation.text
 
-def speak(text, language='tr'):
+def speak(text, language='ar'):
     tts = gTTS(text=text, lang=language, slow=False)
     tts.save("src/output.mp3")
     playsound("./src/output.mp3")  # Added "./" to specify the full path to the file
     os.remove("src/output.mp3")
 
 if __name__ == "__main__":
-    playsound("src/output.mp3")
+    
     while True:
         original_text = sr.__listen__()
         translated_text = translate(original_text)
