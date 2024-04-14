29ed6f31b044370fc8fccab70428cf3f71e09a1b
teymuur <teymur_babayev08@yahoo.com>
2024-04-14 14:14:25 +0400

    Updated

--- tests/3.py
+++ tests/3.py
@@ -1,19 +1,43 @@
+#!/usr/bin/python3
+
+# A small OCR demo. To install pytesseract:
+# sudo apt install -y tesseract-ocr libtesseract-dev
+# pip3 install pytesseract
+
+from pprint import pprint
+
 import cv2
-from picamera2 import Picamera2
+import pytesseract
 
-# Initialize the camera
-camera = Picamera2()
+from picamera2 import MappedArray, Picamera2, Preview
 
-try:
-  # Attempt to capture a frame (basic test)
-  raw_capture = camera.capture_raw(format="bgr")
-except Exception as e:
-  print(f"Error capturing frame: {e}")
-  exit()
-finally:
-  # Close the camera regardless of exceptions 
-  camera.close()
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration({"size": (1024, 768)}))
+picam2.start_preview(Preview.QTGL)
+picam2.start()
 
-# We don't need to decode or process the image data for this test
+threshold = 50
 
-print("picamera2 and cv2 appear to be compatible. No errors encountered during capture.")
+display_data = []
+
+
+def output_text(request):
+    colour = (0, 255, 255)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    with MappedArray(request, "main") as m:
+        for item in display_data:
+            x, y, w, h = item["box"]
+            cv2.putText(m.array, item["text"], (x, y), font, (h + 4) / 35, colour, (h + 12) // 12)
+
+
+picam2.post_callback = output_text
+
+while True:
+    array = picam2.capture_array()
+    data = [line.split('\t') for line in pytesseract.image_to_data(array).split('\n')][1:-1]
+    data = [{"text": item[11], "conf": int(item[10]), "box": (item[6], item[7], item[8], item[9])} for item in data]
+    data = [item for item in data if item["conf"] > threshold and not item["text"].isspace()]
+    for item in data:
+        item["box"] = tuple(map(int, item["box"]))
+    pprint(data, width=500, sort_dicts=False)
+    display_data = data
